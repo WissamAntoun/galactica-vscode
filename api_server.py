e3707374bc17a62a6a158ee95d9330ac642cebd9
ANTOUN Wissam <wissam.antoun@inria.fr>
2023-09-12 10:42:17 +0200

    added caching and bumped version

--- api_server.py
+++ api_server.py
@@ -1,16 +1,24 @@
 import argparse
+import hashlib
 import json
+import logging
 import os
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator, Callable, Dict, Optional, Tuple
 
+import uvicorn
 from fastapi import BackgroundTasks, FastAPI, Request
 from fastapi.responses import JSONResponse, Response, StreamingResponse
-import uvicorn
-
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
+from starlette.requests import Request
+from starlette.responses import Response
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.sampling_params import SamplingParams
 from vllm.utils import random_uuid
+
+logger = logging.getLogger(__name__)
 
 TIMEOUT_KEEP_ALIVE = 5  # seconds.
 TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
@@ -39,38 +47,23 @@ async def health() -> Response:
     return Response(status_code=200)
 
 
-@app.post("/api/generate/")
-async def generate(request: Request) -> Response:
-    """Generate completion for the request.
+def generate_key_builder(
+    func: Callable[..., Any],
+    namespace: str = "",
+    *,
+    request: Optional[Request] = None,
+    response: Optional[Response] = None,
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    cache_key = hashlib.md5(  # noqa: S324
+        f"{func.__module__}:{func.__name__}:{args[1:3]}:{kwargs}".encode()
+    ).hexdigest()
+    return f"{namespace}:{cache_key}"
 
-    The request should be a JSON object with the following fields:
-    - inputs: the inputs to use for the generation.
-    - stream: whether to stream the results or not.
-    - other fields: the sampling parameters (See `SamplingParams` for details).
-    """
-    request_dict = await request.json()
-    inputs = request_dict.pop("inputs")
-    parameters: dict = request_dict.pop("parameters", DEFAULT_PARAMETERS)
 
-    # map parameters to SamplingParams
-    new_parameters = {}
-    for key, value in parameters.items():
-        if key in PARAMETERS_MAPPING:
-            new_parameters[PARAMETERS_MAPPING[key]] = value
-
-    # add default parameters
-    for key, value in DEFAULT_PARAMETERS.items():
-        if key not in new_parameters:
-            new_parameters[key] = value
-
-    if new_parameters.get("temperature", 0.0) <= 0.0:
-        new_parameters["top_k"] = 1  # greedy sampling
-        new_parameters["top_p"] = 1  # greedy sampling
-
-    sampling_params = SamplingParams(**new_parameters)
-
-    # left for the future, but unused in the extension
-    stream = request_dict.pop("stream", False)
+@cache(expire=60 * 60)
+async def _generate(request, inputs, sampling_params, stream):
     request_id = random_uuid()
 
     results_generator = engine.generate(inputs, sampling_params, request_id)
@@ -109,6 +102,44 @@ async def generate(request: Request) -> Response:
     ret = {
         "generated_text": text_outputs[0],
     }
+    return ret
+
+
+@app.post("/api/generate/")
+async def generate(request: Request) -> Response:
+    """Generate completion for the request.
+
+    The request should be a JSON object with the following fields:
+    - inputs: the inputs to use for the generation.
+    - stream: whether to stream the results or not.
+    - other fields: the sampling parameters (See `SamplingParams` for details).
+    """
+    request_dict = await request.json()
+    inputs = request_dict.pop("inputs")
+    parameters: dict = request_dict.pop("parameters", DEFAULT_PARAMETERS)
+
+    # map parameters to SamplingParams
+    new_parameters = {}
+    for key, value in parameters.items():
+        if key in PARAMETERS_MAPPING:
+            new_parameters[PARAMETERS_MAPPING[key]] = value
+
+    # add default parameters
+    for key, value in DEFAULT_PARAMETERS.items():
+        if key not in new_parameters:
+            new_parameters[key] = value
+
+    if new_parameters.get("temperature", 0.0) <= 0.0:
+        new_parameters["top_k"] = 1  # greedy sampling
+        new_parameters["top_p"] = 1  # greedy sampling
+
+    sampling_params = SamplingParams(**new_parameters)
+
+    # left for the future, but unused in the extension
+    stream = request_dict.pop("stream", False)
+
+    ret = await _generate(request, inputs, sampling_params, stream)
+
     return JSONResponse(content=ret)
 
 
@@ -119,6 +150,11 @@ if __name__ == "__main__":
     parser = AsyncEngineArgs.add_cli_args(parser)
     args = parser.parse_args()
 
+    FastAPICache.init(
+        InMemoryBackend(),
+        prefix="fastapi-cache",
+        key_builder=generate_key_builder,
+    )
     engine_args = AsyncEngineArgs.from_cli_args(args)
     engine = AsyncLLMEngine.from_engine_args(engine_args)
 
--- run_api_server_docker.sh
+++ run_api_server_docker.sh
@@ -1,6 +1,6 @@
 MODEL_NAME_OR_PATH=$1
 
-# docker build -t vllm:0.1.6 -f Dockerfile .
+# docker build -t vllm:0.1.7 -f Dockerfile .
 
 docker run \
 --gpus all \
@@ -9,4 +9,4 @@ docker run \
 -p 8000:8000 \
 -v $(pwd):/app \
 -v $MODEL_NAME_OR_PATH:/model_repo \
-vllm:0.1.6 bash -c "cd /app && ./run_api_server.sh /model_repo"
+vllm:0.1.7 bash -c "cd /app && ./run_api_server.sh /model_repo"
